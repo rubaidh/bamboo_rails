0210b2713c1c7b661752b14819b05950aec14f61
Graeme Mathieson <mathie@woss.name>
2009-05-11 17:23:39 +0100

    Specs for (almost) an permutation of project, covering the common cases and the individual nuances of test suite.

--- spec/tasks/tasks_spec.rb
+++ spec/tasks/tasks_spec.rb
@@ -25,39 +25,110 @@ describe "Rake tasks" do
     Object.send :remove_const, :RAILS_ENV
   end
 
-  describe "running the 'bamboo' task" do
-    it "should work" do
-      Rake::Task['bamboo'].invoke.should be_true
-    end
-
+  describe "any bamboo task invocation", :shared => true do
     it "should set RAILS_ENV to 'test'" do
-      Rake::Task['bamboo'].invoke
       ENV["RAILS_ENV"].should == "test"
     end
 
     it "should clear the log files" do
-      Rake::Task['bamboo'].invoke
-      @tasks_run["log:clear"].should be_true
+      @tasks_run.should include("log:clear")
     end
 
     it "should set up the Rails environment" do
-      Rake::Task['bamboo'].invoke
-      @tasks_run["environment"].should be_true
+      @tasks_run.should include("environment")
     end
 
     it "should build any vendor gems" do
-      Rake::Task['bamboo'].invoke
-      @tasks_run["gems:build"].should be_true
+      @tasks_run.should include("gems:build")
     end
 
     it "should set up the Test::Unit ci_reporter reports" do
-      Rake::Task['bamboo'].invoke
-      @tasks_run["ci:setup:testunit"].should be_true
+      @tasks_run.should include("ci:setup:testunit")
     end
 
     it "should run the Test::Unit tests" do
-      Rake::Task['bamboo'].invoke
-      @tasks_run["test"].should be_true
+      @tasks_run.should include("test")
     end
   end
+
+  describe "running the 'bamboo' task" do
+    describe "without any RSpec specs" do
+      before(:each) do
+        Rake::Task['bamboo'].invoke
+      end
+
+      it_should_behave_like "any bamboo task invocation"
+
+      it "should not setup the RSpec reports" do
+        @tasks_run.should_not include("ci:setup:rspec")
+      end
+
+      it "should not run the spec task" do
+        @tasks_run.should_not include("spec:rcov")
+      end
+    end
+
+    describe "without any Cucumber features" do
+      before(:each) do
+        Rake::Task['bamboo'].invoke
+      end
+
+      it_should_behave_like "any bamboo task invocation"
+
+      it "should not setup the Cucumber reports" do
+        @tasks_run.should_not include("ci:setup:cucumber")
+      end
+
+      it "should not run the features task" do
+        @tasks_run.should_not include("features")
+      end
+    end
+
+    describe "with RSpec specs" do
+      before(:each) do
+        [
+          "ci:setup:rspec", "spec:rcov"
+        ].each do |task_name|
+          task task_name do
+            @tasks_run[task_name] = true
+          end
+        end
+        Rake::Task['bamboo'].invoke
+      end
+
+      it_should_behave_like "any bamboo task invocation"
+
+      it "should setup the RSpec reports" do
+        @tasks_run.should include("ci:setup:rspec")
+      end
+
+      it "should run the spec task" do
+        @tasks_run.should include("spec:rcov")
+      end
+    end
+
+    describe "with Cucumber features" do
+      before(:each) do
+        [
+          "ci:setup:cucumber", "features"
+        ].each do |task_name|
+          task task_name do
+            @tasks_run[task_name] = true
+          end
+        end
+        Rake::Task['bamboo'].invoke
+      end
+
+      it_should_behave_like "any bamboo task invocation"
+
+      it "should setup the Cucumber reports" do
+        @tasks_run.should include("ci:setup:cucumber")
+      end
+
+      it "should run the features task" do
+        @tasks_run.should include("features")
+      end
+    end
+  end
+
 end
